501946ed0371272a00ac271f16dd9e92b8c74847
zetton110 <locolv.kohe@gmail.com>
2017-01-30 04:49:36 +0900

    アーティスト情報操作用のAPIの入り口を追加

---
+++ src/jp/setlist/app/api/ArtistApiResource.java
@@ -0,0 +1,18 @@
+package jp.setlist.app.api;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Path;
+
+/**
+ * 
+ * アーティスト情報操作用
+ * WebAPIインターフェース実装クラス
+ * 
+ * @author kohei
+ *
+ */
+@Path("artist")
+@RequestScoped
+public class ArtistApiResource {
+
+}
