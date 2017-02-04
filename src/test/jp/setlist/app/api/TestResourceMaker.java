d91593358823f5dc401b9ab642e889968190b424
zetton110 <locolv.kohe@gmail.com>
2017-02-05 03:42:55 +0900

    テスト用APIがHTTPレスポンスを返却するように修正

--- src/test/jp/setlist/app/api/TestResourceMaker.java
+++ src/test/jp/setlist/app/api/TestResourceMaker.java
@@ -1,12 +1,13 @@
 package jp.setlist.app.api;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
 import jp.setlist.app.common.beans.UserBean;
 import jp.setlist.app.dao.UserInfoDao;
-import jp.setlist.app.dao.UserInfoDaoMapper;
-import jp.setlist.app.dao.impl.UserInfoDaoImpl;
 
 /**
  * テストデータ作成クラス
@@ -15,29 +16,39 @@ import jp.setlist.app.dao.impl.UserInfoDaoImpl;
  *
  */
 @Path("test")
+@RequestScoped
 public class TestResourceMaker {
 
+	/*** ユーザーDAO ***/
+	@Inject
+	UserInfoDao userInfoDao;
+
+	/*** ユーザ情報ビーン ***/
+	@Inject
+	UserBean userBean;
+	
 	/**
 	 * 
 	 * 初期化
 	 * 
 	 * 下記URLへアクセス -> 50件のダミーユーザ情報をDBへ登録
 	 * http://[ホストのIPアドレス]:[ホストのポート番号]/app/test/init
+	 * @return 
 	 * 
 	 */
 	@GET
 	@Path("init")
-	public void init(){
-		
+	public Response init(){
+
 		for(TestDBResourceEnum testUser:TestDBResourceEnum.values()){
-			UserBean userBean = new UserBean();
 			userBean.setId(testUser.getId());
 			userBean.setName(testUser.getName());
-			
-			UserInfoDao userInfoDao = new UserInfoDaoImpl();
 			userInfoDao.insertUser(userBean);
 		}
 		
+		String msg = "ダミーのUSER情報を登録しました。";
+
+		return Response.ok().entity(msg).build();
 	}
 	
 }
